5ca23e28eba077e9874d2a3fcf32eeb43b860cb1
Thomas Latortue <tsl13b@gmail.com>
2018-04-13 16:16:56 -0400

    saving changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-//import { config } from 'dotenv';
+require('dotenv').config();
+// Key for our weather API : process.env.WEATHER_KEY;
+
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${process.env.WEATHER_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    return {
+        type: FETCH_WEATHER
+    }
+}
