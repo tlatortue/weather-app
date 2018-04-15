d0b6821f0c1140806b4cc10973c549732f63179d
Thomas Latortue <tsl13b@gmail.com>
2018-04-15 10:56:38 -0400

    enable ajax request. update packages

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
         this.state = { term: '' };
 
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit  = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -20,6 +21,8 @@ export default class SearchBar extends Component {
         event.preventDefault();
 
         // We need to go and fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
     }
 
     render() {
@@ -42,3 +45,12 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+// make sure function flows to middlewere / reducers
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// export this as default instead of exporting class SearchBar itself
+// null because this container does not care about the state
+export default connect(null, mapDispatchToProps)(SearchBar);
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,9 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  node: {
+    fs: 'empty'
+  },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
