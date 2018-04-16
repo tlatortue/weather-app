48624b1290777c474cd39c744bcb403d0fc631e8
Thomas Latortue <tsl13b@gmail.com>
2018-04-16 17:47:03 -0400

    add google maps integration, add css styles

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component { 
     renderWeather(cityData) {
@@ -11,9 +12,13 @@ class WeatherList extends Component {
         const city_pressure = cityData.list.map(weather => weather.main.pressure);
         const city_humidity = cityData.list.map(weather => weather.main.humidity);
 
+        const lon = cityData.city.coord.lon;
+        const lat = cityData.city.coord.lat;
+        // const { lon, lat } = cityData.city.coord;
+
         return (
             <tr name={city_name} key={cityData.city.id}>
-                <td>{city_name}</td>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
                 <td><Chart data={city_temp} color="red" units="K"/></td>
                 <td><Chart data={city_pressure} color="green" units="hPa"/></td>
                 <td><Chart data={city_humidity} color="gray" units="%"/></td>
