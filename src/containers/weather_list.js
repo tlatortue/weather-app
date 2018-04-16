02e2e8786d9d92d9b821e3d429bd57782bdfb11b
Thomas Latortue <tsl13b@gmail.com>
2018-04-15 23:42:14 -0400

    add styles, graphs, displaying of rows properly

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+
+import Chart from '../components/chart';
 
 class WeatherList extends Component { 
     renderWeather(cityData) {
+        const city_name = cityData.city.name;
+
+        const city_temp     = cityData.list.map(weather => weather.main.temp);
+        const city_pressure = cityData.list.map(weather => weather.main.pressure);
+        const city_humidity = cityData.list.map(weather => weather.main.humidity);
+
         return (
-            <tr>
-                <td>{cityData.city.name}</td>
+            <tr name={city_name} key={cityData.city.id}>
+                <td>{city_name}</td>
+                <td><Chart data={city_temp} color="red" units="K"/></td>
+                <td><Chart data={city_pressure} color="green" units="hPa"/></td>
+                <td><Chart data={city_humidity} color="gray" units="%"/></td>
             </tr>
         );
     }
@@ -16,9 +27,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
